cd0ccdc850943b6ec87ff107fe14935472956866
Tamara Marnell <tmarnell@orbiscascade.org>
2022-03-24 12:46:38 -0700

    Changed deletion date from NOW() to -30 days

--- tools/delete-tokens.php
+++ tools/delete-tokens.php
@@ -3,7 +3,7 @@
 require_once(getenv('AW_HOME') . '/defs.php');
 include(AW_INCLUDES . '/server-header.php');
 if ($mysqli = connect()) {
-  $mysqli->query('DELETE FROM oai WHERE expiration < NOW()');
+  $mysqli->query('DELETE FROM oai WHERE expiration < "' . date('Y-m-d H:i:s', strtotime('-30 days')) . '"');
   $mysqli->close();
 }
 ?>
