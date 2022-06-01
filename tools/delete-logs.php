9914f4ffc787b0bd407e122b4ab4f333d5c3938f
Tamara Marnell <tmarnell@orbiscascade.org>
2022-06-01 08:47:46 -0700

    Added script to delete BaseX data logs older than 1 month

---
+++ tools/delete-logs.php
@@ -0,0 +1,8 @@
+<?php
+// Clear older BaseX data logs
+require_once(getenv('AW_HOME') . '/defs.php');
+include(AW_INCLUDES . '/server-header.php');
+
+$last_month = strtotime('-1 month');
+exec('rm /opt/basex/data/.logs/' . date('Y-m', $last_month) . '*.log');
+?>
