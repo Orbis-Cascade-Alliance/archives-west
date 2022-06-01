9abe5712a9a33c0be0c91263fb5cfcafe4d69f3a
Tamara Marnell <tmarnell@orbiscascade.org>
2022-06-01 09:17:23 -0700

    Changed month definition

--- tools/delete-logs.php
+++ tools/delete-logs.php
@@ -3,6 +3,6 @@
 require_once(getenv('AW_HOME') . '/defs.php');
 include(AW_INCLUDES . '/server-header.php');
 
-$last_month = strtotime('-1 month');
+$last_month = strtotime('-2 months');
 exec('rm /opt/basex/data/.logs/' . date('Y-m', $last_month) . '*.log');
 ?>
