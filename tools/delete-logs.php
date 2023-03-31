97fad06ff9e4e6eca7a739a89a6857f3f5aa5595
Tamara Marnell <tmarnell@orbiscascade.org>
2023-03-31 13:35:30 -0700

    Remove scripts that should be run by root only

--- tools/delete-logs.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Clear older BaseX data logs
-require_once(getenv('AW_HOME') . '/defs.php');
-include(AW_INCLUDES . '/server-header.php');
-
-$last_month = strtotime('-2 months');
-exec('rm /opt/basex/data/.logs/' . date('Y-m', $last_month) . '*.log');
-?>
--- tools/delete-tokens.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Delete expired OAI-PMH resumption tokens
-require_once(getenv('AW_HOME') . '/defs.php');
-include(AW_INCLUDES . '/server-header.php');
-if ($mysqli = connect()) {
-  $mysqli->query('DELETE FROM oai WHERE expiration < "' . date('Y-m-d H:i:s', strtotime('-30 days')) . '"');
-  $mysqli->close();
-}
-?>
