b8e98519c7deacfd0b59e87239e30017e38ab542
Tamara Marnell <tmarnell@orbiscascade.org>
2025-06-10 09:28:56 -0700

    Update global CSS version

--- includes/header.php
+++ includes/header.php
@@ -4,7 +4,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta name="description" content="Archives West provides access to descriptions of primary sources in the western United States, including correspondence, diaries, and photographs. Digital reproductions of primary sources are available in some cases. Archives West was formerly known as the Northwest Digital Archives (NWDA).">
-    <link rel="stylesheet" href="<?php echo AW_DOMAIN; ?>/layout/global.css?v=1.1" />
+    <link rel="stylesheet" href="<?php echo AW_DOMAIN; ?>/layout/global.css?v=1.2" />
     <link rel="stylesheet" href="<?php echo AW_DOMAIN; ?>/layout/print.css" />
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="<?php echo AW_DOMAIN; ?>/scripts/global.js"></script>
